7a4d2f4ed9e3138ba98ceedd582a278f49f1dd81
Diya <gdiya4@gmail.com>
2025-05-20 23:30:28 +0530

    commit

---
+++ src/products.js
@@ -0,0 +1,73 @@
+import product1 from "./assets/download.jpeg";
+import product2 from "./assets/download (1).jpeg";
+import product3 from "./assets/download (2).jpeg";
+import product4 from "./assets/download (3).jpeg";
+import product5 from "./assets/download (4).jpeg";
+import product6 from "./assets/download (5).jpeg";
+import product7 from "./assets/download (6).jpeg";
+import product8 from "./assets/download (7).jpeg";
+import product9 from "./assets/download (8).jpeg";
+import product10 from "./assets/download (9).jpeg";
+
+export const PRODUCTS = [
+    { 
+        id: 1,
+        productName: "Carry Bag",
+        price: 500,
+        productImage: product1,
+    },
+    {
+        id: 2,
+        productName: "Smart Watch",
+        price: 10000,
+        productImage: product2,
+    },
+    {
+        id: 3,
+        productName: "Sports Shoes",
+        price: 4000,
+        productImage: product3,
+    },
+    {
+        id: 4,
+        productName: "Iphone",
+        price: 50000,
+        productImage: product4,
+    },
+    {
+        id: 5,
+        productName: "Facewash",
+        price: 200,
+        productImage: product5,
+    },
+    {
+        id: 6,
+        productName: "Top",
+        price: 700,
+        productImage: product6,
+    },
+    {
+        id: 7,
+        productName: "Lipstick",
+        price: 300,
+        productImage: product7,
+    },
+    {
+        id: 8,
+        productName: "Macbook",
+        price: 80000,
+        productImage: product8,
+    },
+    {
+        id: 9,
+        productName: "OnePlus Earpods",
+        price: 12000,
+        productImage: product9,
+    },
+    {
+        id: 10,
+        productName: "Canon Camera",
+        price: 40000,
+        productImage: product10,
+    },
+];
